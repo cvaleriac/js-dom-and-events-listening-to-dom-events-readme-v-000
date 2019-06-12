4812e2924ae1f280e40cf1a6afc5904a38f3d6d4
valeria campilongo <cvaleriac@hotmail.it>
2019-06-12 12:15:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -7,11 +7,11 @@ main.addEventListener('click', function(event) {
 });
 
 divs = document.querySelectorAll('div');
- 
+
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   // set the third argument to `true`!
   divs[i].addEventListener('click', capture, true);
